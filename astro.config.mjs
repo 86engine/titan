9c8d04804534fdd3739429138a1cb502bb7c3e14
“wenziwell” <wenziwell@163.com>
2026-06-07 10:36:51 +0800

    性能优化：删除11个无用JS引用，PurgeCSS清理冗余CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import purgecss from 'astro-purgecss';
 
 export default defineConfig({
-
-
+  site: 'https://titan-recycling.com',
+  integrations: [
+    purgecss(),
+  ],
 });
